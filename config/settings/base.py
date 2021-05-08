a2b6e16e4284aab9fbd996d254972dbfcee5f5be
Julio Hurtado (Juliens) <juliens@outlook.com>
2021-05-08 16:41:51 -0500

    :bug: Fix ckeditor toolbar

--- config/settings/base.py
+++ config/settings/base.py
@@ -200,3 +200,33 @@ TEMPLATE_WIDGETS = {
     "url": "widgets/textinput.html",
 }
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+CKEDITOR_CONFIGS = {
+    "default": {
+        "skin": "moono-lisa",
+        "toolbar_Basic": [["Source", "-", "Bold", "Italic"]],
+        "toolbar_Full": [
+            ["JustifyLeft", "JustifyCenter", "JustifyRight", "JustifyBlock"],
+            [
+                "Styles",
+                "Format",
+                "Bold",
+                "Italic",
+                "Underline",
+                "Strike",
+                "SpellChecker",
+                "Undo",
+                "Redo",
+            ],
+            ["Link", "Unlink", "Anchor"],
+            ["Image", "Flash", "Table", "HorizontalRule"],
+            ["TextColor", "BGColor"],
+            ["Smiley", "SpecialChar"],
+            ["Source"],
+        ],
+        "toolbar": "Full",
+        "height": 291,
+        "width": 1400,
+        "filebrowserWindowWidth": 940,
+        "filebrowserWindowHeight": 725,
+    }
+}
