45bd68e1652a88444d95d8bfd90c594ad9a8104f
Julio Hurtado (Juliens) <juliens@outlook.com>
2022-03-18 21:27:55 -0500

    :ambulance: Fix util detail

--- apps/utils/mixins.py
+++ apps/utils/mixins.py
@@ -10,4 +10,4 @@ from superadmin.templatetags.superadmin_utils import site_url
 class LinkDetailMixin:
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        return redirect(site_url(self.object, "detail"))
+        return redirect(self.object.destination)
