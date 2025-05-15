2083994d98e352d7bb5930fa17ae89e9f632b52e
Julio Hurtado <juliens@outlook.com>
2025-05-15 17:45:05 -0500

    chore: remove old levels update code

--- apps/dynamicforms/urls.py
+++ apps/dynamicforms/urls.py
@@ -1,11 +1,7 @@
-# Django
 from django.urls import path
 
-# Views
 from .views import (
     CountMonthlyPostsForm,
-    GetVault,
-    UpdateLevelsForm,
     UpdatePostForm,
     UpdateProfileForm,
     UpdateTopicsForm,
@@ -16,11 +12,6 @@ urlpatterns = [
         route="dynamicforms/data/<int:pk>/",
         view=UpdateProfileForm.as_view(),
         name="update_profile",
-    ),
-    path(
-        route="dynamicforms/levels/<int:pk>/",
-        view=UpdateLevelsForm.as_view(),
-        name="update_levels",
     ),
     path(
         route="dynamicforms/topics/<int:pk>/",
@@ -36,10 +27,5 @@ urlpatterns = [
         route="dynamicforms/posts/<int:pk>/",
         view=CountMonthlyPostsForm.as_view(),
         name="count_posts",
-    ),
-    path(
-        route="dynamicforms/vaults/update/",
-        view=GetVault.as_view(),
-        name="update_vaults",
     ),
 ]
